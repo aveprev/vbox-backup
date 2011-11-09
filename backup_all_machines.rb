83984927506ddc0b9b3f92dfc5b58f86465c370b
Andrei Veprev <andrei.veprev@objectstyle.com>
2011-11-09 16:13:30 +0300

    Name parameter for machine clone
    Generating name for machine clone using timestamp string.

--- backup_all_machines.rb
+++ backup_all_machines.rb
@@ -4,8 +4,16 @@ def get_available_machine_names
   `VBoxManage list vms`.lines.map { |l| l.scan(/".*"/)[0].slice(1..-2) }
 end
 
-def generate_snapshot_name
+def generate_timestamp_string
   Time.new.strftime("%Y-%m-%d %H:%M:%S")
+end
+
+def generate_snapshot_name
+  generate_timestamp_string
+end
+
+def generate_machine_clone_name(machine_name)
+  machine_name + " " + generate_timestamp_string
 end
 
 def take_snapshot(machine_name, snapshot_name)
@@ -13,7 +21,8 @@ def take_snapshot(machine_name, snapshot_name)
 end
 
 def clone_machine(machine_name, snapshot_name)
-  `VBoxManage clonevm \"#{machine_name}\" --snapshot \"#{snapshot_name}\" --mode machineandchildren --basefolder #{BACKUP_DIR}`
+  machine_clone_name = generate_machine_clone_name(machine_name)
+  `VBoxManage clonevm \"#{machine_name}\" --snapshot \"#{snapshot_name}\" --name \"#{machine_clone_name}\" --mode machineandchildren --basefolder #{BACKUP_DIR}`
 end
 
 def delete_snapshot(machine_name, snapshot_name)
