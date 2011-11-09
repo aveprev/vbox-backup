0db2c4957c1173e979e270afcde746c697f6f1c0
Andrei Veprev <andrei.veprev@objectstyle.com>
2011-11-09 15:44:48 +0300

    Using machine names instead of ids

--- backup_all_machines.rb
+++ backup_all_machines.rb
@@ -1,33 +1,33 @@
 BACKUP_DIR = "/srv/vm/Backup/"
 
-def get_avaliable_machine_ids
-  `VBoxManage list vms`.lines.map { |l| l.scan(/\{.*\}/)[0].slice(1..-2) }
+def get_available_machine_names
+  `VBoxManage list vms`.lines.map { |l| l.scan(/".*"/)[0].slice(1..-2) }
 end
 
 def generate_snapshot_name
   Time.new.strftime("%Y-%m-%d %H:%M:%S")
 end
 
-def take_snapshot(machine_id, snapshot_name)
-  `VBoxManage snapshot #{machine_id} take \"#{snapshot_name}\" --pause`
+def take_snapshot(machine_name, snapshot_name)
+  `VBoxManage snapshot \"#{machine_name}\" take \"#{snapshot_name}\" --pause`
 end
 
-def clone_machine(machine_id, snapshot_name)
-  `VBoxManage clonevm #{machine_id} --snapshot \"#{snapshot_name}\" --mode machineandchildren --basefolder #{BACKUP_DIR}`
+def clone_machine(machine_name, snapshot_name)
+  `VBoxManage clonevm \"#{machine_name}\" --snapshot \"#{snapshot_name}\" --mode machineandchildren --basefolder #{BACKUP_DIR}`
 end
 
-def delete_snapshot(machine_id, snapshot_name)
-  `VBoxManage snapshot #{machine_id} delete \"#{snapshot_name}\"`
+def delete_snapshot(machine_name, snapshot_name)
+  `VBoxManage snapshot \"#{machine_name}\" delete \"#{snapshot_name}\"`
 end
 
-def do_backup(machine_ids)
-  machine_ids.each do |id|
+def do_backup(machine_names)
+  machine_names.each do |name|
     snapshot_name = generate_snapshot_name
-    take_snapshot(id, snapshot_name)
-    clone_machine(id, snapshot_name)
-    delete_snapshot(id, snapshot_name)
+    take_snapshot(name, snapshot_name)
+    clone_machine(name, snapshot_name)
+    delete_snapshot(name, snapshot_name)
   end
 end
 
-do_backup(get_avaliable_machine_ids)
+do_backup(get_avaliable_machine_names)
 
