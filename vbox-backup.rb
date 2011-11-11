2ec74ff0b7a6dc18d33a30a49cd7c19c4db39553
Andrei Veprev <andrei.veprev@objectstyle.com>
2011-11-11 11:51:10 +0300

    Parameter for backups output directory
    Moved output directory parameter from constant to script argument. Added usage information for script.

--- vbox-backup.rb
+++ vbox-backup.rb
@@ -1,5 +1,6 @@
+require "optparse"
+
 VBOX_MANAGE = "/usr/bin/VBoxManage"
-BACKUP_DIR = "/srv/vm/Backup/"
 
 def exec_vbox_manage(*params)
   params.map! { |p| p.include?("\s") ? "\"#{p}\"" : p }
@@ -29,24 +30,44 @@ def take_snapshot(machine_name, snapshot_name)
   exec_vbox_manage "snapshot", machine_name, "take", snapshot_name, "--pause"
 end
 
-def clone_machine(machine_name, snapshot_name)
+def clone_machine(machine_name, snapshot_name, output_dir)
   machine_clone_name = generate_machine_clone_name(machine_name)
   exec_vbox_manage "clonevm", machine_name, "--snapshot", snapshot_name, "--name",
-      machine_clone_name, "--mode", "machine", "--basefolder", BACKUP_DIR
+      machine_clone_name, "--mode", "machine", "--basefolder", output_dir
 end
 
 def delete_snapshot(machine_name, snapshot_name)
   exec_vbox_manage "snapshot", machine_name, "delete", snapshot_name
 end
 
-def do_backup(machine_names)
+def do_backup(machine_names, output_dir)
   machine_names.each do |name|
     snapshot_name = generate_snapshot_name
     take_snapshot(name, snapshot_name)
-    clone_machine(name, snapshot_name)
+    clone_machine(name, snapshot_name, output_dir)
     delete_snapshot(name, snapshot_name)
   end
 end
 
-do_backup(get_available_machine_names)
+def do_backup_al(output_dir)
+  do_backup(get_available_machine_names, output_dir)
+end
 
+options = {}
+optparser = OptionParser.new do |opts|
+  opts.banner = "Usage: vbox-backup.rb -d DIRECTORY"
+  opts.on("-d", "--directory DIRECTORY", "Output DIRECTORY where backup clones will be placed") do |dir|
+    options[:output_dir] = dir
+  end
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end
+optparser.parse!
+
+if options[:output_dir].nil?
+  puts optparser
+else
+  do_backup_all options[:output_dir]
+end
